06166097b144a24160ff64a45cb4765aa2f770f0
Tiago Serpa <clashrocker@gmail.com>
2020-05-03 16:54:15 -0300

    Configurações iniciais

---
+++ main.py
@@ -0,0 +1,9 @@
+"""
+Cólera Punk 1 - por: Tiago77 - 03/05/2020
+Jogo de plataforma com temática de gangues e culturas de rua, inspirado nos coletivos e gangues de Punks do Brasil
+"""
+
+import pygame
+import os
+
+
